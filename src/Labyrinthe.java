769dde7b112a8d49bd8554d87bf1eefa981f1bce
Mash Lahache <Famik_345@hotmail.com>
2020-12-10 21:18:20 -0500

    added pseudocode to file.

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -21,32 +21,80 @@ public class Labyrinthe{
 
     //resout le labyrinthe
     public static boolean resoudre(int[][] grille){
+
+
+
+
+
+        /*
+
+        //marking of coordinates
+        visited
+        deadend
+        exit
+
+
+        find the start coordinate from the first line
+        find the exit coordinate from the last line
+
+        if no exit
+            return unsolvable cause of no exit
+
+        recursif
+
+        //cas de base
+        if current location == exit
+            return current location //found the exit
+            change affichage des cases visited
+
+        //autres cas
+
+        check down first
+        check right
+        check left
+        check up
+
+        if never went
+            mark current coordinate as visited
+            go to next coordinates
+
+        if wall or deadend
+            mark current coordinate as deadend
+            go back to last visited
+
+         */
+
+
+
+
+
+
+
+        int x=0;
+        int y=0;
+
+        int left = grille[x-1][y];
+
+
+        boolean trouve;
+        int deadend = -1;
+        boolean isValid = y >= 0 || y < grille.length || x >= 0 || x < grille[y].length; ;
+
+        if (y < 0 || y >= grille.length || x < 0 || x >= grille.length)
+
+
+
+        for (x=0;x< grille.length;x++){
+
+            for (y = 0; y< grille[x].length; y++)
+                if (grille[x][y] == 0){
+                    //TODO check if deadend, if so change value for deadend.
+                    if (grille[x][y] != left){
+
+                    }
+                }
+        }
+
         return true;
     }
-
-    //1.Find deadends
-        //a. if i != 1 then check with this index
-        //b. if index is surrounded by 1 == deadends (change index to -1)
-        //c. comeback to last index (move index by 1)
-
-    //2.fill from deadends until any junction
-
-    //3. whats left is solution
-
-    /*
-
-    110111
-    110101
-    100001
-    111101
-
-      check ton premier if en premier
-      when index(0) found check if index(-1),index(+1) is another 0
-      if no index(0) around == deadends
-      traceback until junction found two 0.
-
-     */
-
-
-
 }
